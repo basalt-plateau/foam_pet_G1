731b17134a3ac490af2386ab8b3f0ae3684c67ad
friends <friends>
2024-10-08 03:25:49 +0000

    tenets

---
+++ estate/tenents/check_rules.py
@@ -0,0 +1,19 @@
+
+
+
+''''
+	requirements map:
+		pip install pipdeptree
+	
+		pipdeptree --json-tree
+		
+	include LICENSE with pip:
+		https://pypi.org/project/pip-licenses/
+	
+		pip-licenses --with-license-file --format=plain > licenses.txt
+		
+		pip-licenses --with-license-file --format=json > /Metro/vehicles/foam_pet/Rules/Laboratory/PyPI_tenets_full.json
+		pip-licenses > /Metro/vehicles/foam_pet/Rules/Laboratory/PyPI_tenets_outline.txt
+		
+		pipdeptree -d 
+"'''
